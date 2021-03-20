09504bc2f6ed3f068027abcaffbdb47181d4bd20
adriancastro626 <adriancastro626@icloud.com>
2021-03-20 22:05:43 +0000

    excuseGenerator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generarExcusa();
+  });
+  console.log("Hello Rigo from the console! ");
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generarExcusa = () => {
+  let pronoun = ["El", "Un", "Este", "Ese", "Mi"];
+  let subject = ["perro", "gato", "mono", "elefante"];
+  let action = ["rompio mi", "se comio mi", "se escapo de la", "se perdio mi"];
+  let thing = ["tarea", "codigo", "papel", "comida"];
+  let where = ["en la casa", "en el mall", "en el zoo", "en el bar"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let thingIndex = Math.floor(Math.random() * thing.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    thing[thingIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
